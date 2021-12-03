b4bb12fc592c9f52664a6bc96eb60522404a70a5
songHyeongwon <iks1681@naver.com>
2021-12-03 20:43:05 +0900

    리덕스 개발 툴 적용

--- learning-react/react-redux-tutorial/src/index.js
+++ learning-react/react-redux-tutorial/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer , composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store= {store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
