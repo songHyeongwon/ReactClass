271c953fa4efecbcc080217ec3069519db022c56
songHyeongwon <iks1681@naver.com>
2021-12-10 19:51:08 +0900

    20장 서버사이드 랜더링

---
+++ learning-react/ssr-recipe/src/components/Blue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blue.css'
+
+const Blue = () => {
+    return (
+        <div className='Blue'>
+            red
+        </div>
+    );
+};
+
+export default Blue;
---
+++ learning-react/ssr-recipe/src/components/Red.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Red.css'
+
+const Red = () => {
+    return (
+        <div className='Red'>
+            red
+        </div>
+    );
+};
+
+export default Red;
