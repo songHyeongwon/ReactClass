b4debe4a5eab78905b3fe0d711d203113d3862ba
songHyeongwon <iks1681@naver.com>
2021-12-12 13:32:36 +0900

    20장 리코딩중

--- learning-react/ssr-recipe2/config/paths.js
+++ learning-react/ssr-recipe2/config/paths.js
@@ -68,6 +68,8 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
   publicUrlOrPath,
+  ssrIndexJs : resolveApp('src/index.server.js'), //서버 사이드 랜더링 엔트리
+  ssrBuild : resolveApp('dist') //웹팩 처리 후 저장 경로
 };
 
 
--- learning-react/ssr-recipe2/src/index.js
+++ learning-react/ssr-recipe2/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./modules";
+
+const store = createStore(
+  rootReducer,
+  window.__PRELOADED_STATE__,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
