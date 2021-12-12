0056aba5aeb591e6ba127a0a70f5451a9b59aad8
songHyeongwon <iks1681@naver.com>
2021-12-12 13:06:07 +0900

    20 서버 사이드 랜더링

---
+++ learning-react/ssr-recipe2/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage';
+//import UsersPage from './pages/UsersPage';
+
+const App = () => {
+  return (
+    <div>
+      <Menu/>
+      <hr/>
+      <Route path="/red" component={RedPage}></Route>
+      <Route path="/blue" component={BluePage}></Route>
+      {/* <Route path="/users" component={UsersPage}></Route> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ learning-react/ssr-recipe2/src/components/Blue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blue.css'
+
+const Blue = () => {
+    return (
+        <div className='Blue'>
+            blue
+        </div>
+    );
+};
+
+export default Blue;
