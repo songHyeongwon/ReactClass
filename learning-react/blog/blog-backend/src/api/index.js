a4b9759de47321d80a499f390be2dd7d2bd984e4
songHyeongwon <iks1681@naver.com>
2021-12-28 21:57:04 +0900

    모든 모듈을 import방식으로 변경

--- learning-react/blog/blog-backend/src/api/index.js
+++ learning-react/blog/blog-backend/src/api/index.js
@@ -1,6 +1,7 @@
-const Router = require('koa-router');
-const posts = require('./posts');
-
+//const Router = require('koa-router');
+//const posts = require('./posts');
+import Router from "koa-router";
+import posts from './posts';
 const api = new Router();
 //라우터 설정
 api.use('/posts', posts.routes());
@@ -8,4 +9,5 @@ api.use('/posts', posts.routes());
 // api.get('/test', (ctx) => {
 //   ctx.body = '테스트성공';
 // });
-module.exports = api;
+//module.exports = api;
+export default api;
