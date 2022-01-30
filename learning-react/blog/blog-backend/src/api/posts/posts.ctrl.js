dc310249ba4470294fb8bd5a479e6e1128a40751
songHyeongwon <iks1681@naver.com>
2022-01-30 14:32:11 +0900

    요청데이터 정합성 검사까지

--- learning-react/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ learning-react/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,11 +1,12 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
 
 const { ObjectId } = mongoose.Types;
 
 export const checkObjectId = (ctx, next) => {
   const { id } = ctx.params;
-  if(!ObjectId.isValid(id)) {
+  if (!ObjectId.isValid(id)) {
     ctx.status = 400; //잘못된 ID
     return;
   }
@@ -13,6 +14,18 @@ export const checkObjectId = (ctx, next) => {
 };
 
 export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    //객체가  다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), //required() 필수값
+    body: Joi.string().required(), //required() 필수값
+    tags: Joi.array().items(Joi.string()).required(), //배열이고 안에 아이템은 스트링이고 + 필수값
+  });
+  const result = schema.validate(ctx.request.body);
+  if(result.error) {
+    ctx.status = 400; //잘못된 요청
+    ctx.body = result.error
+    return;
+  }
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -26,6 +39,7 @@ export const write = async (ctx) => {
     ctx.throw(500, e);
   }
 };
+
 export const list = async (ctx) => {
   try {
     const posts = await Post.find().exec();
@@ -34,6 +48,7 @@ export const list = async (ctx) => {
     ctx.throw(500, e);
   }
 };
+
 export const read = async (ctx) => {
   const { id } = ctx.params;
   try {
@@ -47,6 +62,7 @@ export const read = async (ctx) => {
     ctx.throw(500, e);
   }
 };
+
 export const remove = async (ctx) => {
   const { id } = ctx.params;
   try {
@@ -56,8 +72,23 @@ export const remove = async (ctx) => {
     ctx.throw(500, e);
   }
 };
+
 export const update = async (ctx) => {
   const { id } = ctx.params;
+  //작성과 비슷 다만 필수가 아닌게 있음 타입만 검사함
+  const schema = Joi.object().keys({
+    title: Joi.string(), //required() 필수값
+    body: Joi.string(), //required() 필수값
+    tags: Joi.array().items(Joi.string()), //배열이고 안에 아이템은 스트링이고 + 필수값
+  });
+
+  //검증실패 에러일 경우
+  const result = schema.validate(ctx.request.body);
+  if(result.error) {
+    ctx.status = 400; //잘못된요청
+    ctx.body = result.error;
+    return;
+  }
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // false 일경우에는이전것을 반환합니다.
