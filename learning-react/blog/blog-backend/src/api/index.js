c9437d57ccfec87b37c61fd36eb54461061077ff
songHyeongwon <iks1681@naver.com>
2022-01-31 13:42:17 +0900

    라우터 잡기

--- learning-react/blog/blog-backend/src/api/index.js
+++ learning-react/blog/blog-backend/src/api/index.js
@@ -2,9 +2,11 @@
 //const posts = require('./posts');
 import Router from "koa-router";
 import posts from './posts';
+import auth from './auth';
 const api = new Router();
 //라우터 설정
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 // api.get('/test', (ctx) => {
 //   ctx.body = '테스트성공';
