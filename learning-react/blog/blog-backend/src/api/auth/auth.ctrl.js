3702633f33c0eaa199bd0e79b25b0bf1e71cdfa9
songHyeongwon <iks1681@naver.com>
2022-02-01 13:06:00 +0900

    로그인 확인까지

--- learning-react/blog/blog-backend/src/api/auth/auth.ctrl.js
+++ learning-react/blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -1,12 +1,70 @@
-export const register = async ctx => {
-    //회원가입
+import Joi from '@hapi/joi';
+import User from '../../models/user';
+
+export const register = async (ctx) => {
+  //회원가입
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+  try {
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      // 중복인 유저네임이 있다
+      ctx.status = 409;
+      return;
+    }
+
+    const user = new User({
+      username,
+    });
+    await user.setPassword(password); //비밀번호 해시로
+    await user.save(); //저장
+
+    //응답객체에 비밀번호 제외
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
-export const login = async ctx => {
-    //로그인   
+export const login = async (ctx) => {
+  //로그인
+  const { username, password } = ctx.request.body;
+  //네임과 패스워드 필드만 체크
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    //계정 없으면 에러
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password); //비밀번호 체크
+    //잘못된 암호
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
-export const check = async ctx => {
-    //상태체크
+export const check = async (ctx) => {
+  //상태체크
 };
-export const logout = async ctx => {
-    //로그아웃
+export const logout = async (ctx) => {
+  //로그아웃
 };
--- learning-react/blog/blog-backend/src/models/user.js
+++ learning-react/blog/blog-backend/src/models/user.js
@@ -6,19 +6,25 @@ const UserScheam = new Schema({
   hashedPassword: String,
 });
 
-UserScheam.method.setPassword = async function (password) {
+UserScheam.methods.setPassword = async function (password) {
   const hash = await bcrypt.hash(password, 10);
   //this를 사용하기 위해 애로우 펑션 사용 X
   this.hashedPassword = hash;
 };
 
-UserScheam.method.checkPassword = async function (password) {
+UserScheam.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword);
   return result;
 };
 
 UserScheam.statics.findByUsername = function(username) {
     return this.findOne({username});
+}
+
+UserScheam.methods.serialize = function() {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
 }
 
 const User = mongoose.model('User', UserScheam);
